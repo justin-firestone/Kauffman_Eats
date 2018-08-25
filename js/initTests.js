23c6dfb2a164825e7cd7f1f4bf220f682b6ccb02
Reid Jones <reid.jones@huskers.unl.edu>
2018-08-25 14:58:49 -0500

    Added getNewStartLocation and getNewEndLocation unit tests

--- js/initTests.js
+++ js/initTests.js
@@ -34,7 +34,7 @@ QUnit.test("addDeliverators: Add 10 Deliverators", function(assert){
     assert.equal(deliverators.length, numDeliverators);
 });
 
-QUnit.test("addDeliverators: Add Negative Deliverators", function(assert){
+QUnit.test("addDeliverators: Add negative Deliverators", function(assert){
     //arrange
     let numDeliverators = -10;
     let expectedNum = 0;
@@ -44,4 +44,69 @@ QUnit.test("addDeliverators: Add Negative Deliverators", function(assert){
 
     //assert
     assert.equal(deliverators.length, expectedNum);
+});
+
+QUnit.test("getNewStartLocation: Deliverator with already-set end location", function(assert){
+    //arrange
+    let expectedStartId = 844923267;
+    let node = {
+        id: 'test_deliverator',
+        label: 'T_D',
+        color: {
+            background: getRandomColor(),
+        },
+        group: 'deliverator',
+        start: false,
+        restaurant: false,
+        end: expectedStartId
+    };
+
+    //act
+    let newStart = getNewStartLocation(node);
+
+    //assert
+    assert.equal(newStart.id, expectedStartId);
+});
+
+QUnit.test("getNewStartLocation: Deliverator with no end location", function(assert){
+    //arrange
+    let node = {
+        id: 'test_deliverator',
+        label: 'T_D',
+        color: {
+            background: getRandomColor(),
+        },
+        group: 'deliverator',
+        start: false,
+        restaurant: false,
+        end: false
+    };
+
+    //act
+    let newStart = getNewStartLocation(node);
+
+    //assert
+    assert.ok(newStart.id);
+});
+
+QUnit.test("getNewEndLocation: Deliverator with already-set start location", function(assert){
+    //arrange
+    let expectedEndId = 844923267;
+    let node = {
+        id: 'test_deliverator',
+        label: 'T_D',
+        color: {
+            background: getRandomColor(),
+        },
+        group: 'deliverator',
+        start: expectedEndId,
+        restaurant: false,
+        end: false
+    };
+
+    //act
+    let newStart = getNewEndLocation(node);
+
+    //assert
+    assert.ok(newStart.id !== expectedEndId);
 });
